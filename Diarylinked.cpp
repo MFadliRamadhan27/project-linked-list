ccca3f07c4229047f4c09896610d7a52334f2168
M.Fadli Ramadhan <fadli0127mfr@gmail.com>
2025-06-28 18:20:21 +0800

    lanjutan inisialisasi dan fungsi

--- Diarylinked.cpp
+++ Diarylinked.cpp
@@ -7,3 +7,16 @@ struct Catatan {
 };
 
 Catatan* head = NULL;
+void tambahCatatan(string isi) {
+    Catatan* baru = new Catatan{isi, NULL};
+    if (head == NULL) {
+        head = baru;
+    } else {
+        Catatan* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = baru;
+    }
+    cout << "Catatan ditambahkan: \"" << isi << "\"\n";
+}
