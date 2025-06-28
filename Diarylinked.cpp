875efa50042061aac1db15b51142e50f2e80a85f
M.Fadli Ramadhan <fadli0127mfr@gmail.com>
2025-06-28 18:29:53 +0800

    tambahkan menu utama dan integrasi semua fungsi

--- Diarylinked.cpp
+++ Diarylinked.cpp
@@ -62,3 +62,47 @@ void hitungCatatan() {
     }
     cout << "Jumlah catatan: " << jumlah << endl;
 }
+int main() {
+    int pilihan;
+    string isiCatatan;
+
+    do {
+        cout << "\n=== MENU CATATAN HARIAN ===\n";
+        cout << "1. Tambah Catatan\n";
+        cout << "2. Tampilkan Semua Catatan\n";
+        cout << "3. Hapus Catatan Pertama\n";
+        cout << "4. Hapus Semua Catatan\n";
+        cout << "5. Hitung Catatan\n";
+        cout << "0. Keluar\n";
+        cout << "Pilihan: ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1:
+                cout << "Ketik catatan: ";
+                cin >> ws;
+                getline(cin, isiCatatan);
+                tambahCatatan(isiCatatan);
+                break;
+            case 2:
+                tampilkanCatatan();
+                break;
+            case 3:
+                hapusCatatanPertama();
+                break;
+            case 4:
+                hapusSemuaCatatan();
+                break;
+            case 5:
+                hitungCatatan();
+                break;
+            case 0:
+                cout << "Keluar dari program.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid.\n";
+        }
+    } while (pilihan != 0);
+
+    return 0;
+}
